2710edef95679046b6659c01b2797a6d1d376d78
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-26 04:47:05 +0600

    dot.js file added

---
+++ dot.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Kodom Ali',
+    job: 'Van Chala',
+    3: 'third',
+    'add-ress': 'pat khet'
+}
+const prop = 'name';
+console.log(person.name);
+console.log(person['job']);
+console.log(person[prop]);
+console.log(person['3']);
+console.log(person['add-ress']);
